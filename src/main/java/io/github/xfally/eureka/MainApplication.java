0ba1c3db3c7e9d0e19cc811f37d391cd91120e77
pax <coolwinding@gmail.com>
2020-09-19 21:10:26 +0800

    Adjust groupId

--- src/main/java/io/github/xfally/eureka/MainApplication.java
+++ src/main/java/io/github/xfally/eureka/MainApplication.java
@@ -1,4 +1,4 @@
-package com.example.eureka;
+package io.github.xfally.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class MainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication.run(MainApplication.class, args);
 	}
 
 }
--- src/test/java/io/github/xfally/eureka/MainApplicationTests.java
+++ src/test/java/io/github/xfally/eureka/MainApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.eureka;
+package io.github.xfally.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaApplicationTests {
+class MainApplicationTests {
 
 	@Test
 	void contextLoads() {
